d5577870c98b8f8f3fab7c4da74e68644818eff5
Andreas Wahlberg <andreas.wahlberg@live.se>
2015-01-07 13:32:56 +0100

    initial

---
+++ Labb2_NivaA/Labb2_NivaA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_Niva_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int farg = 25;
+                
+                for (int rad = 0; <  25; rad ++)
+            {
+                if (rad % 2 = 1)
+                {Console.Write(" ")}
+            }
+
+
+        }
+    }
+}
