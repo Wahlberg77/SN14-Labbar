989fab68e5ada110d9478c8db4f040772455a6dc
Andreas Wahlberg <andreas.wahlberg@live.se>
2015-02-06 13:53:37 +0100

    Check v 1.0

--- Labb2_Niva_B/Program.cs
+++ Labb2_Niva_B/Program.cs
@@ -5,63 +5,82 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Labb2_Niva_B
-{                       //-----------------------------------------------------------------------------------
-    //-------------------------Andreas Wahlberg Labb 2 Nivå B----------------------------
-    //-----------------------------------------------------------------------------------
+{
+                           //-------------------------Andreas Wahlberg Labb 2 Nivå B----------------------------
+
     class Program
     {
         const string title = "Ange det udda antalet asterisker (max 79) i triangelns bas: ";
+        const int BaseMax = 79;
+        const int BaseMin = 1;
 
-        //Denna metod ska anropa metoderna ReadOddByte och RenderTriangle. Anropen ska placeras
-        //i en ”do-while”-sats som avslutas då användaren trycker på Escape-tangenten
         private static void Main(string[] args)
-            {
-                byte basen = ReadOddByte(title);
-                //DrawTriangle(basen);
-
-               
+        {
             do
-                   { 
-                        Console.WriteLine();
-                        Console.BackgroundColor = ConsoleColor.DarkGreen;
-                        Console.WriteLine("Tryck valfri tangent för en ny beräkning - ESC avslutar.");
-                        Console.ResetColor();
+            {
+                Console.Clear();
+                byte basen = ReadOddByte(title);
+                RenderTriangle(basen);
 
-                    } 
-                        while (Console.ReadKey().Key != ConsoleKey.Escape);
-                            Console.Clear();
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("\n\nTryck valfri tangent för en ny beräkning - ESC eller hårt slag \nav hammare avslutar.");
+                Console.ResetColor();
             }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
 
-        //Returnerar ett udda heltal av typen byte.
-        //Felhantering och ligger i intervallet 1 - 79. 
-        //Visa felmeddelande och göra en ny inmatning igen.
         private static byte ReadOddByte(string title)
         {
             byte number = 0;
-
+            string strTest = "";
             while (true)
             {
-                byte readOddByte =0;
-                //number = byte.Parse(strTest);
+                Console.Write(title);
+                strTest = Console.ReadLine();
 
-                if (readOddByte < 1 && readOddByte > 79 && number % 2 == 0)
+                byte.TryParse(strTest, out number);
+                if ((number > BaseMax) || ((number % 2) != 1))
                 {
-                    //Fixa udda talet med modulus och kasta undantag.
                     Console.WriteLine();
                     Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine("Fel! Det inmatade värdet är inte ett udda heltal mellan 1 och 79.");
                     Console.ResetColor();
+                }
+                else
+                {
+                    return number;
                 }
             }
         }
 
-        //parametern cols som ger antalet asterisker triangelns bas ska innehålla. 
-        //Använd nässlade for-satser. 
         private static void RenderTriangle(byte cols)
         {
-            string strTest = ("");
-            string stars = ("*");
-            Console.WriteLine("fis" ,stars);
+            int CountRows = (cols + 1) / 2;
+            int iCount = (cols - 1) / 2;
+            int printCount = 1;
+
+            for (int row = 0; row < CountRows; row++)
+            {
+                Console.WriteLine();
+
+                for (int column = 0; column < iCount; column++)
+                {
+                    Console.Write(" ");
+                }
+                iCount--;
+
+                for (int column = 0; column < printCount; column++)
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write("*");
+                    Console.ResetColor();
+                }
+                printCount += 2;
+            }
+            return;
         }
     }
 }
